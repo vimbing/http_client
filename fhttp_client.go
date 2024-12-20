862d17d9f11bd1d311ba6b0d9751caafac011cd7
vimbing <jakubbubak9@gmail.com>
2024-12-20 12:18:38 +0100

    changes

--- fhttp_client.go
+++ fhttp_client.go
@@ -12,6 +12,47 @@ import (
 	"golang.org/x/net/proxy"
 )
 
+func socksDialer(pickedProxy string) (proxy.ContextDialer, error) {
+	proxyUrl, err := url.Parse(pickedProxy)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var auth *proxy.Auth
+
+	if proxyUrl.User != nil && len(proxyUrl.User.Username()) > 0 {
+		password, passwordSet := proxyUrl.User.Password()
+
+		auth = &proxy.Auth{
+			User: proxyUrl.User.Username(),
+		}
+
+		if passwordSet {
+			auth.Password = password
+		}
+	}
+
+	dialSocksProxy, err := proxy.SOCKS5("tcp", proxyUrl.Host, auth, &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var ok bool
+
+	dialer, ok := dialSocksProxy.(proxy.ContextDialer)
+
+	if !ok {
+		return nil, errors.New("failed type assertion to DialContext")
+	}
+
+	return dialer, nil
+}
+
 func newFhttpClient(cfg *Config) (*http.Client, error) {
 	client := &http.Client{
 		Timeout: cfg.timeout,
@@ -30,42 +71,7 @@ func newFhttpClient(cfg *Config) (*http.Client, error) {
 		pickedProxy := cfg.proxies[RandomInt(0, len(cfg.proxies))]
 
 		if strings.Contains(cfg.proxies[0], "socks") {
-			proxyUrl, err := url.Parse(pickedProxy)
-
-			if err != nil {
-				return client, err
-			}
-
-			var auth *proxy.Auth
-
-			if proxyUrl.User != nil && len(proxyUrl.User.Username()) > 0 {
-				password, passwordSet := proxyUrl.User.Password()
-
-				auth = &proxy.Auth{
-					User: proxyUrl.User.Username(),
-				}
-
-				if passwordSet {
-					auth.Password = password
-				}
-			}
-
-			dialSocksProxy, err := proxy.SOCKS5("tcp", proxyUrl.Host, auth, &net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-			})
-
-			if err != nil {
-				return client, err
-			}
-
-			var ok bool
-
-			dialer, ok = dialSocksProxy.(proxy.ContextDialer)
-
-			if !ok {
-				return client, errors.New("failed type assertion to DialContext")
-			}
+			dialer, err = socksDialer(pickedProxy)
 		} else {
 			dialer, err = newConnectDialer(pickedProxy)
 		}
