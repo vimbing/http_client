0fadd0712eafb3ed9e87300497d32f73dc09c9e7
vimbing <jakubbubak9@gmail.com>
2025-10-06 23:27:15 +0200

    Implemented new set of tests for ja3 checking

--- client_test.go
+++ client_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 	"testing"
 	"time"
@@ -41,11 +42,110 @@ func chrome140Profile() TlsProfile {
 }
 
 type testClientTlsResponse struct {
-	Ja4 string `json:"ja4"`
+	Ja4        string `json:"ja4"`
+	AkamaiHash string `json:"akamai_hash"`
+	Ja3Text    string `json:"ja3_text"`
+}
+
+type ja3 struct {
+	sslVersion               string
+	ciphers                  []string
+	sslExtensions            []string
+	ellipticCurvePoints      []string
+	ellipticCurvePointFormat string
+}
+
+func parseJa3Text(ja3Text string) *ja3 {
+	ja3Parts := strings.Split(ja3Text, ",")
+
+	sslVersion := ja3Parts[0]
+	cipher := ja3Parts[1]
+	sslExtension := ja3Parts[2]
+	ellipticCurve := ja3Parts[3]
+	ellipticCurvePointFormat := ja3Parts[4]
+
+	return &ja3{
+		sslVersion:               sslVersion,
+		ciphers:                  strings.Split(cipher, "-"),
+		sslExtensions:            strings.Split(sslExtension, "-"),
+		ellipticCurvePoints:      strings.Split(ellipticCurve, "-"),
+		ellipticCurvePointFormat: ellipticCurvePointFormat,
+	}
+}
+
+func (j *ja3) compareWithExpected(expected *ja3, t *testing.T) {
+	if j.sslVersion != expected.sslVersion {
+		t.Errorf("Undexpected ssl version in ja3, expected: %s, got: %s", expected.sslVersion, j.sslVersion)
+	}
+
+	if j.ellipticCurvePointFormat != expected.ellipticCurvePointFormat {
+		t.Errorf("Undexpected eliptic point format in ja3, expected: %s, got: %s", expected.ellipticCurvePointFormat, j.ellipticCurvePointFormat)
+	}
+
+	findAdditionalAndMissing := func(expected, actual []string) (missing []string, additional []string) {
+		for _, cipher := range actual {
+			if !slices.Contains(expected, cipher) {
+				additional = append(additional, cipher)
+			}
+		}
+
+		for _, cipher := range expected {
+			if !slices.Contains(actual, cipher) {
+				missing = append(missing, cipher)
+			}
+		}
+
+		return
+	}
+
+	unexpectedMissingCiphers, unexpectedAdditionalCiphers := findAdditionalAndMissing(
+		expected.ciphers,
+		j.ciphers,
+	)
+
+	if len(unexpectedMissingCiphers) > 0 {
+		t.Errorf("Found unexpected missing ciphers in ja3: [%s]", strings.Join(unexpectedMissingCiphers, ", "))
+	}
+
+	if len(unexpectedAdditionalCiphers) > 0 {
+		t.Errorf("Found unexpected additional ciphers in ja3: [%s]", strings.Join(unexpectedAdditionalCiphers, ", "))
+	}
+
+	unexpectedMissingEllipticCurvePoints, unexpectedAdditionalEllipticCurvePoints := findAdditionalAndMissing(
+		expected.ellipticCurvePoints,
+		j.ellipticCurvePoints,
+	)
+
+	if len(unexpectedMissingEllipticCurvePoints) > 0 {
+		t.Errorf("Found unexpected missing elliptic curves point in ja3: [%s]", strings.Join(unexpectedMissingEllipticCurvePoints, ", "))
+	}
+
+	if len(unexpectedAdditionalEllipticCurvePoints) > 0 {
+		t.Errorf("Found unexpected additional elliptic curves point in ja3: [%s]", strings.Join(unexpectedAdditionalEllipticCurvePoints, ", "))
+	}
+
+	unexpectedMissingSSLExtensions, unexpectedAdditionalMissingSSLExtensions := findAdditionalAndMissing(
+		expected.sslExtensions,
+		j.sslExtensions,
+	)
+
+	if len(unexpectedMissingSSLExtensions) > 0 {
+		t.Errorf("Found unexpected missing ssl extensions in ja3: [%s]", strings.Join(unexpectedMissingSSLExtensions, ", "))
+	}
+
+	if len(unexpectedAdditionalMissingSSLExtensions) > 0 {
+		t.Errorf("Found unexpected additional ssl extensions in ja3: [%s]", strings.Join(unexpectedAdditionalMissingSSLExtensions, ", "))
+	}
+
 }
 
 func TestClientTLSChrome140(t *testing.T) {
-	expectedJa4 := "t13d1516h2_8daaf6152771_02713d6af862"
+	// t13d1516h2_8daaf6152771_02713d6af862 maybe this as well
+	expectedJa4 := "t13d1516h2_8daaf6152771_d8a2da3f94cd"
+	expectedAkamaiHash := "52d84b11737d980aef856699f885ca86"
+	expectedJaTextString := "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,11-51-43-5-16-23-65281-0-45-35-65037-27-18-17613-13-10,4588-29-23-24,0"
+
+	expectedJa3 := parseJa3Text(expectedJaTextString)
 
 	client := MustNew(
 		WithInsecureSkipVerify(),
@@ -53,7 +153,8 @@ func TestClientTLSChrome140(t *testing.T) {
 	)
 
 	res, err := client.Get(
-		"https://tls.browserleaks.com/",
+		"https://tls.browserleaks.com/json",
+		http.Header{http.PHeaderOrderKey: {":method", ":authority", ":scheme", ":path"}},
 	)
 
 	if err != nil {
@@ -67,8 +168,15 @@ func TestClientTLSChrome140(t *testing.T) {
 		t.Fatalf("Error while binding response body: %v", err)
 	}
 
+	clientJa3 := parseJa3Text(body.Ja3Text)
+	clientJa3.compareWithExpected(expectedJa3, t)
+
 	if body.Ja4 != expectedJa4 {
-		t.Fatalf("Client has unexpected ja4, expected: %s got %s", expectedJa4, body.Ja4)
+		t.Errorf("Client has unexpected ja4, expected: %s got %s", expectedJa4, body.Ja4)
+	}
+
+	if body.AkamaiHash != expectedAkamaiHash {
+		t.Errorf("Client has unexpected akamai hash, expected: %s got %s", expectedAkamaiHash, body.AkamaiHash)
 	}
 }
 
@@ -91,7 +199,7 @@ func TestGetRequest(t *testing.T) {
 	}
 
 	if res.BodyString() != "pong" {
-		t.Fatalf("Unexpected body from test server: %s", res.BodyString())
+		t.Errorf("Unexpected body from test server: %s", res.BodyString())
 	}
 }
 
@@ -135,7 +243,7 @@ func TestPostRequest(t *testing.T) {
 		bodyString := res.BodyString()
 
 		if bodyString != testCase.expected {
-			t.Fatalf("Server responded with unexpected body, expected %s got %s", testCase.expected, bodyString)
+			t.Errorf("Server responded with unexpected body, expected %s got %s", testCase.expected, bodyString)
 		}
 	}
 }
@@ -228,7 +336,7 @@ func TestProxyRotation(t *testing.T) {
 		t.Logf("Ip service responded with ip: %s", res.BodyString())
 
 		if lastAddress == res.BodyString() {
-			t.Fatalf("Proxy repeated after rotation: %s", lastAddress)
+			t.Errorf("Proxy repeated after rotation: %s", lastAddress)
 		}
 	}
 }
